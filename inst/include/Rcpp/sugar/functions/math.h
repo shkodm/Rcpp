490a710282870c8ac636c8978c6495f8c0b46e0e
Dmytro Sashko <dmshko@gmail.com>
2025-04-29 11:54:28 +1000

    Adjust for admclang compiler
    clang 17 - 18 does not like implicit function signatures...

--- inst/include/Rcpp/sugar/functions/math.h
+++ inst/include/Rcpp/sugar/functions/math.h
@@ -22,24 +22,43 @@
 #ifndef RCPP_SUGAR_MATH_H
 #define RCPP_SUGAR_MATH_H
 
-VECTORIZED_MATH_1(exp,::exp)
-VECTORIZED_MATH_1(acos,::acos)
-VECTORIZED_MATH_1(asin,::asin)
-VECTORIZED_MATH_1(atan,::atan)
-VECTORIZED_MATH_1(ceil,::ceil)
-VECTORIZED_MATH_1(ceiling,::ceil)
-VECTORIZED_MATH_1(cos,::cos)
-VECTORIZED_MATH_1(cosh,::cosh)
-VECTORIZED_MATH_1(floor,::floor)
-VECTORIZED_MATH_1(log,::log)
-VECTORIZED_MATH_1(log10,::log10)
-VECTORIZED_MATH_1(sqrt,::sqrt)
-VECTORIZED_MATH_1(sin,::sin)
-VECTORIZED_MATH_1(sinh,::sinh)
-VECTORIZED_MATH_1(tan,::tan)
-VECTORIZED_MATH_1(tanh,::tanh)
+inline double rcpp_log(double x) { return ::log(x); }
+inline double rcpp_log10(double x) { return ::log10(x); }
+inline double rcpp_sqrt(double x) { return ::sqrt(x); }
+inline double rcpp_sin(double x) { return ::sin(x); }
+inline double rcpp_sinh(double x) { return ::sinh(x); }
+inline double rcpp_tan(double x) { return ::tan(x); }
+inline double rcpp_tanh(double x) { return ::tanh(x); }
+inline double rcpp_fabs(double x) { return ::fabs(x); }
+inline double rcpp_expm1(double x) { return ::expm1(x); }
+inline double rcpp_log1p(double x) { return ::log1p(x); }
+inline double rcpp_cos(double x) { return ::cos(x); }
+inline double rcpp_cosh(double x) { return ::cosh(x); }
+inline double rcpp_floor(double x) { return ::floor(x); }
+inline double rcpp_ceil(double x) { return ::ceil(x); }
+inline double rcpp_exp(double x) { return ::exp(x); }
+inline double rcpp_acos(double x) { return ::acos(x); }
+inline double rcpp_asin(double x) { return ::asin(x); }
+inline double rcpp_atan(double x) { return ::atan(x); }
 
-VECTORIZED_MATH_1(abs,::fabs)
+VECTORIZED_MATH_1(exp,rcpp_exp)
+VECTORIZED_MATH_1(acos,rcpp_acos)
+VECTORIZED_MATH_1(asin,rcpp_asin)
+VECTORIZED_MATH_1(atan,rcpp_atan)
+VECTORIZED_MATH_1(ceil,rcpp_ceil)
+VECTORIZED_MATH_1(ceiling,rcpp_ceil)
+VECTORIZED_MATH_1(cos,rcpp_cos)
+VECTORIZED_MATH_1(cosh,rcpp_cosh)
+VECTORIZED_MATH_1(floor,rcpp_floor)
+VECTORIZED_MATH_1(log,rcpp_log)
+VECTORIZED_MATH_1(log10,rcpp_log10)
+VECTORIZED_MATH_1(sqrt,rcpp_sqrt)
+VECTORIZED_MATH_1(sin,rcpp_sin)
+VECTORIZED_MATH_1(sinh,rcpp_sinh)
+VECTORIZED_MATH_1(tan,rcpp_tan)
+VECTORIZED_MATH_1(tanh,rcpp_tanh)
+
+VECTORIZED_MATH_1(abs,rcpp_fabs)
 
 VECTORIZED_MATH_1(gamma      , ::Rf_gammafn     )
 VECTORIZED_MATH_1(lgamma     , ::Rf_lgammafn    )
@@ -47,8 +66,8 @@ VECTORIZED_MATH_1(digamma    , ::Rf_digamma     )
 VECTORIZED_MATH_1(trigamma   , ::Rf_trigamma    )
 VECTORIZED_MATH_1(tetragamma , ::Rf_tetragamma  )
 VECTORIZED_MATH_1(pentagamma , ::Rf_pentagamma  )
-VECTORIZED_MATH_1(expm1      , ::expm1          )
-VECTORIZED_MATH_1(log1p      , ::log1p          )
+VECTORIZED_MATH_1(expm1      , rcpp_expm1          )
+VECTORIZED_MATH_1(log1p      , rcpp_log1p          )
 
 namespace Rcpp{
     namespace internal{
